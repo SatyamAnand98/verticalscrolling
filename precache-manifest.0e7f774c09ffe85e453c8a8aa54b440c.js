e804612ad29e595cb29ecf5cb9fd5b578569ece8
SatyamAnand98 <thesatemail@gmail.com>
2020-08-01 17:42:48 +0530

    Updates

---
+++ precache-manifest.0e7f774c09ffe85e453c8a8aa54b440c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53de1575ee1db7c5d00effdf6065781c",
+    "url": "/verticalscrolling/index.html"
+  },
+  {
+    "revision": "fb121291e0a0f7e29356",
+    "url": "/verticalscrolling/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1402733905f8347d8b97",
+    "url": "/verticalscrolling/static/css/main.a9274c44.chunk.css"
+  },
+  {
+    "revision": "fb121291e0a0f7e29356",
+    "url": "/verticalscrolling/static/js/2.65408416.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/verticalscrolling/static/js/2.65408416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1402733905f8347d8b97",
+    "url": "/verticalscrolling/static/js/main.0b63ce7c.chunk.js"
+  },
+  {
+    "revision": "f73008783122fdd2f48a",
+    "url": "/verticalscrolling/static/js/runtime-main.6b28c959.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/verticalscrolling/static/media/logo.5d5d9eef.svg"
+  }
+]);
